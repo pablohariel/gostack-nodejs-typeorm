c6f37c952b5bafb665b57c3702972f9011cc19a3
pablohariel <pablohhdr@gmail.com>
2021-05-18 16:06:49 -0300

    Desafio completado

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,26 +1,57 @@
 import { Router } from 'express';
+import multer from 'multer';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import { getCustomRepository } from 'typeorm';
+
+import uploadConfig from '../config/upload';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
+const upload = multer(uploadConfig);
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transationRepository = getCustomRepository(TransactionsRepository);
+  const transactions = await transationRepository.find({
+    relations: ['category'],
+  });
+  const balance = await transationRepository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category = 'others' } = request.body;
+  const createTransaction = new CreateTransactionService();
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const deleteTransaction = new DeleteTransactionService();
+  const transaction = await deleteTransaction.execute(id);
+  return response.json(transaction);
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const { path } = request.file;
+    const importTransactions = new ImportTransactionsService();
+    const transactions = await importTransactions.execute(path);
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,55 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const caterogyRepository = getRepository(Category);
+
+    if (
+      type === 'outcome' &&
+      (await transactionsRepository.getBalance()).total < value
+    ) {
+      throw new AppError('Account does not have enough money');
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    const checkCategoryExists = await caterogyRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!checkCategoryExists) {
+      const newCategory = caterogyRepository.create({
+        title: category,
+      });
+      await caterogyRepository.save(newCategory);
+      transaction.category = newCategory;
+    } else {
+      transaction.category = checkCategoryExists;
+    }
+
+    await transactionsRepository.save(transaction);
+    return transaction;
   }
 }
 
